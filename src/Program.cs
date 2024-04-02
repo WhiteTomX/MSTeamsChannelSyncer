907cce4793742ab20eee859586bc79e69117ebb6
WhiteTom <whitetom@arcor.de>
2024-04-02 21:56:14 +0200

    fix: Dependencies

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,25 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using WhiteTom.MsTeamsChannelSyncer.Services;
+using WhiteTom.MsTeamsChannelSyncer.Repositories;
+using Microsoft.Graph;
+using Azure.Identity;
+
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
-    .ConfigureServices(services => {
+    .ConfigureServices(services =>
+    {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddSingleton<GraphServiceClient>(_ =>
+        {
+            return new GraphServiceClient(new DefaultAzureCredential());
+        });
+        services.AddSingleton<IConfigurationRepository, ConfigurationRepository>();
+        services.AddScoped<IChannelRepository, GraphChannelRepository>();
+        services.AddScoped<ISynchronizationService, GraphSynchronizationService>();
     })
     .Build();
 
